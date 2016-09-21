93b963f7c477b872c9b3412d77c446222a10642a
Sudheer Chekka <sudheer.chekka@gmail.com>
2016-09-20 22:53:15 -0700

    cleaned

--- src/main/java/com/mycompany/camel/spring/VaccinationReportTransformer.java
+++ src/main/java/com/mycompany/camel/spring/VaccinationReportTransformer.java
@@ -27,12 +27,10 @@ public class VaccinationReportTransformer{
 			}, new Output(Output.IO_STREAM) {
 				@Override
 				public OutputStream getStream() throws Exception {
-					//return super.getStream();
 					return out;
 				}
 			});
 			
-			//mapping2XML.run("/tmp/hl7multi_v02_v04.hl7", "/tmp/MyCustomReport/hl7multi_v02_v04.out.hl7");
 		} catch (Exception e) {
 			e.printStackTrace();
 			throw new IllegalStateException("could not run mapping", e);
@@ -40,28 +38,6 @@ public class VaccinationReportTransformer{
 
 		return new String(out.toByteArray());
 	}
-
-//	String elementName = "return";
-//	public String transform(Element from){	
-//		MappingMapToVaccinationQueryReport mapping2XML = new MappingMapToVaccinationQueryReport();
-//		String output = "MyCustomReport";
-//        String input = null;
-//
-//		NodeList nodes =	from.getElementsByTagName(elementName);
-//		if(nodes.getLength()>0){
-//			input = nodes.item(0).getChildNodes().item(0).getNodeValue();
-//			try {
-//				mapping2XML.run(input, output);
-//			} catch (Exception e) {
-//				e.printStackTrace();
-//			}
-//		}
-//	
-//
-//
-//			return output;
-//
-//	}
 }
 
 
